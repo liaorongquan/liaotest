d935cf41e70d9e2969f882c8696075a87c1a9eca
liaorongquan <2628248115@qq.com>
2018-10-08 22:35:05 +0800

    1222

---
+++ 计算器公式.sql
@@ -0,0 +1,23 @@
+select * from prj_project t where t.project_number = 'GFZL201809017';
+--项目报价
+select pq.calc_session_id,pq.* from prj_quotation pq where pq.document_id = 1681
+--以下为计算器四张表
+--计算器头
+select * from hls_fin_calculator_hd hf where hf.calc_session_id = 901;
+--计算器明细
+select * from hls_fin_calculator_ln hc where hc.calc_session_id = 901;
+--计算器头公式
+select * from hls_fin_calculator_hd_formula h where h.calc_session_id = 901;
+--计算器明细公式
+select hl.other_fee2,hl.* from hls_fin_calculator_ln_formula hl where hl.calc_session_id = 901;
+--在项目报价阶段 更改了报价器配置，点公式重算。  公式会覆盖。
+
+--
+select c.calc_session_id,c.* from con_contract c where c.contract_number = 'GFZL0756201804702' and c.data_class = 'NORMAL'
+--计算器头
+select * from hls_fin_calculator_hd hf where hf.calc_session_id = 902;
+--计算器明细
+select * from hls_fin_calculator_ln hc where hc.calc_session_id = 904;
+--计算器头公式
+select * from hls_fin_calculator_hd_formula h where h.calc_session_id = 902;
+select hl.other_fee2,hl.* from hls_fin_calculator_ln_formula hl where hl.calc_session_id = 904 for update;
